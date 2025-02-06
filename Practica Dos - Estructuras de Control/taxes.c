0e491c83174a24c38f2c1b44ef62857c2e4468ba
Giowans <gio_tails@live.com>
2025-02-05 22:52:11 -0600

    Empezamos la practica dos

---
+++ Practica Dos - Estructuras de Control/taxes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**IMPUESTOS:
+ *
+ * Ayudar al usuario a determinar cuánto debe pagar de impuestos dependiendo de su salario.
+ */
+
+int main()
+{
+    // Your code here
+    printf("Hello, World!\n");
+    return 0;
+}
