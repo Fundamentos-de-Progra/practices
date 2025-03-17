ac1656beb9985bd52fdfe56194b7b48b65437968
unknown <gio_tails@live.com>
2025-03-17 14:17:06 -0600

    anadiendo una estructura repetitiva para el menu

--- Practica Dos - Estructuras de Control/examplefornancy.c
+++ Practica Dos - Estructuras de Control/examplefornancy.c
@@ -19,11 +19,71 @@ void determinarBisiesto(char nombreUser[20]) {
     system("pause");
 }
 
+// Programa que saluda a una personita segun un menu
+void saludarPersonita(char nombreUser[20]) {
+    int opcion = 0;
+    printf("\t\t\tHola %s esto es: SALUDAR A UNA PERSONITA\n\n", nombreUser);
+    printf("Ingresa el numero de la opcion que quieres hacer: \n\t1-.) Saludar a Laura \n\t2-.) Saludar a Carlos \n\t3-.) Saludar a Victoria \n\t4-.) Saludar a Lic. Espinoza uwu \n\t5-.) Saludar a Noe\n\nTu opcion: ");
+    scanf("%i", &opcion);
+    switch (opcion)
+    {
+        case 1:
+            printf("\n\n**** Holi Laura uwu ****");
+            break;
+        case 2:
+            printf("\n\n**** Holi Carlos uwu ****");
+            break;
+        case 3:
+            printf("\n\n**** Holi Victoria n.n/ ****");
+            break;
+        case 4:
+            printf("\n\n**** Holi Lic. Espinoza :D ****");
+            break;
+        case 5:
+            printf("\n\n**** Holi Noe (Don Nancy <333) ****");
+            break;
+        default:
+            printf("\n\nopcion invalida u.u");
+            break;
+    }
+    printf("\n");
+    system("pause");
+}
+
+// El programa principal ahora nos dara la capacidad de seleccionar que programa ejecutar.
+
 int main() {
     char nombreUsuario[20];
+    int opc_main = 0;
     printf("Ingresa tu nombre: ");
     gets(nombreUsuario);
-    system("cls");
-    determinarBisiesto(nombreUsuario);
+    printf("\n\n\t\t****_____-- Bienvenido %s --____****\n\n", nombreUsuario);
+    while (opc_main != 3 )
+    {
+        /* code */
+        printf("\nIngresa una opcion para: \n\t1-. Ejecutar a%co bisiesto \n\t2-. Ejecutar saludar a personitas \n\t3-. Salir", 164);
+        printf("\ntu opcion: ");
+        scanf("%i", &opc_main);
+        switch (opc_main)
+        {
+            case 1:
+                system("cls");
+                determinarBisiesto(nombreUsuario);
+                system("cls");
+                break;
+            case 2:
+                system("cls");
+                saludarPersonita(nombreUsuario);
+                system("cls");
+                break;
+            case 3:
+                printf("\n\nGracias por usarme uwu\n");
+                break;
+            default:
+                printf("\nopcion invalida u.u");
+                break;
+        }
+    }
+    system("pause");
     return 0;
 }
